4255c9d01a0b690c52ba479b038c297f2bfb6d5f
Connell Boyce <connellboyce17@gmail.com>
2026-02-15 14:05:19 -0500

    fix: Remove version endpoint from security policy

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/((?!api/auth|_next|favicon.ico).*)"],
+    matcher: ["/((?!api/auth|_next|stats/version|favicon.ico).*)"],
 };
