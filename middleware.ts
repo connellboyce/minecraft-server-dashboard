98bd66119603505cd2bf5fe7791f9d413540f6ae
Connell Boyce <connellboyce17@gmail.com>
2026-02-15 13:48:38 -0500

    fix: Add filtering for auth for all endpoints

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,5 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-    pages: {
-        signIn: '/api/auth/signin/connellboyce',
-    },
-    callbacks: {
-        authorized: ({ token }) => {
-            return !!token;
-        },
-    },
-});
+export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|api/auth).*)',
-    ],
+    matcher: ["/((?!api/auth|_next|favicon.ico).*)"],
 };
-
